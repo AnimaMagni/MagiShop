c12059da709156e83116a5ecfb9ab636392e9022
AnimaMagni <animamagni@gmail.com>
2026-06-30 16:44:09 +0330

    add product details

--- shop/urls.py
+++ shop/urls.py
@@ -2,12 +2,21 @@ from django.urls import path
 
 from .views import (
     store,
-    product_detail
+    product_detail,
+    home
+    
 )
 
 urlpatterns = [
+
     path(
         "",
+        home,
+        name="home"
+    ),
+
+    path(
+        "store/",
         store,
         name="store"
     ),
--- shop/views.py
+++ shop/views.py
@@ -1,14 +1,46 @@
 from django.shortcuts import render, get_object_or_404
+from .models import Product, Category
 
-from .models import Product
 
 
 def store(request):
-    products = Product.objects.filter(is_featured=True)
-    return render(request, "store/store.html", {"products": products})
+
+    categories = Category.objects.all()
+
+    category_slug = request.GET.get("category")
+
+    if category_slug:
+        products = products.filter(
+            category__slug=category_slug
+        )
+
+    products = Product.objects.filter(
+        is_featured=True
+    )
+
+
+    return render(
+        request,
+        "store/store.html",
+        {
+            "products": products,
+            "categories": categories,
+        }
+    )
 
 
 #def product_detail(request, id):
 def product_detail(request, slug):
-    product = get_object_or_404(Product, slug=slug)
-    return render(request, "store/product_detail.html", {"product": product})
+
+    product = get_object_or_404(
+        Product,
+        slug=slug
+    )
+
+    return render(
+        request,
+        "store/product_detail.html",
+        {
+            "product": product
+        }
+    )
