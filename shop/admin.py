8dcda0820e0271503d556692946a3a5c6036805f
AnimaMagni <animamagni@gmail.com>
2026-07-08 18:10:19 +0330

    MagiShop v1.0

--- shop/admin.py
+++ shop/admin.py
@@ -1,12 +1,33 @@
-from .models import Category, Contact
+from .models import (
+    Product,
+    Category,
+    Contact,
+) 
 
 from django.contrib import admin
 
-from .models import Product
 
-from .models import Contact
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name',
+                    'category',
+                    'price',
+                    'is_featured',
+                    'created_at'
+                    )
+    search_fields = ('name', 'description','category__name')
+    list_filter = ('category', 'is_featured', 'created_at')
+    ordering = ('created_at',)
+    prepopulated_fields = {'slug': ('name',)}
 
 
-admin.site.register(Contact)
-admin.site.register(Product)
-admin.site.register(Category)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'subject',)
+    search_fields = ('name', 'email', 'subject')
